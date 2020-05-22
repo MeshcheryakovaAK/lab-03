df9c6938a316462e43eea578883ed957d85bc4c7
Alexandra(MeshcheryakovaAK) <alexandra@example.com>
2020-05-22 23:39:26 +0300

    New: добавлены тесты (все проходят)

--- histogram.cpp
+++ histogram.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 void find_minmax(vector<double> numbers, double& min, double& max)
 {
+    if (numbers.size()==0) return;
     min = numbers[0];
     max = numbers[0];
     for (double number : numbers)
