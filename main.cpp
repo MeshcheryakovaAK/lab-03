dcf911d13870e422e52eea6a3065d098253974af
Alexandra(MeshceryakovaAK) <alexandra@example.com>
2020-05-24 15:56:35 +0300

    new: лр4 ч1 выполнена

--- main.cpp
+++ main.cpp
@@ -41,6 +41,13 @@ Input read_input(istream& in, bool prompt)
     return data;
 }
 
+size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+    size_t data_size = item_size * item_count;
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    (*buffer).write(reinterpret_cast<const char*>(items), data_size);
+    return data_size;
+}
+
 Input download(const string& address)
 {
     stringstream buffer;
@@ -49,7 +56,9 @@ Input download(const string& address)
     if (curl)
     {
         CURLcode res;
-        curl_easy_setopt(curl, CURLOPT_URL, address);
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
         res = curl_easy_perform(curl);
         if (res)
         {
@@ -64,9 +73,12 @@ Input download(const string& address)
 int main(int argc, char* argv[])
 {
     Input input;
-    if (argc > 1) {
+    if (argc > 1)
+    {
         input = download(argv[1]);
-    } else {
+    }
+    else
+    {
         input = read_input(cin, true);
     }
     const auto bins = make_histogram(input);
