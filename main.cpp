cb370a93c1b1c6567fe9ccfdc1894bab713003cb
Alexandra(MeshceryakovaAK) <alexandra@example.com>
2020-05-24 15:26:49 +0300

    new: выполнен пункт 3.2.6

--- main.cpp
+++ main.cpp
@@ -43,14 +43,17 @@ int main(int argc, char* argv[])
 {
     if (argc > 1)
     {
-        cout << argc << endl;
-        for (int i = 0; i < argc; i++)
+        curl_global_init(CURL_GLOBAL_ALL);
+        CURL *curl = curl_easy_init();
+        if (curl)
         {
-            cout << argv[i] << endl;
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
         }
     }
     return 0;
-    curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
