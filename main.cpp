9eadb6d60b5a5ca5047dcdbc66a84aa93ecd1475
Alexandra(MeshceryakovaAK) <alexandra@example.com>
2020-05-24 14:39:50 +0300

    new: ввод данных как структуры

--- main.cpp
+++ main.cpp
@@ -2,7 +2,14 @@
 #include <vector>
 #include "histogram.h"
 #include "for_svg.h"
+
 using namespace std;
+
+struct Input
+{
+    vector<double> numbers;
+    size_t bin_count;
+};
 
 vector<double> input_numbers(istream& in, size_t count)
 {
@@ -13,24 +20,32 @@ vector<double> input_numbers(istream& in, size_t count)
     }
     return result;
 }
-int main()
-{
 
-    size_t number_count;
+Input read_input(istream& in)
+{
+    Input data;
+
     cerr << "Enter number count: ";
-    cin >> number_count;
+    size_t number_count;
+    in >> number_count;
 
     cerr << "Enter numbers: ";
-    const auto numbers=input_numbers(cin, number_count);
+    data.numbers = input_numbers(in, number_count);
 
-    size_t bin_count;
     cerr << "Enter column count: ";
-    cin >> bin_count;
+    in >> data.bin_count;
+
+    return data;
+}
+
+int main()
+{
+    Input data = read_input(cin);
 
     double min,max;
-    find_minmax(numbers,min,max);
+    find_minmax(data.numbers, min, max);
 
-    const auto bins = make_histogram(numbers, bin_count,min,max);
+    const auto bins = make_histogram(data.numbers, data.bin_count, min, max);
     show_histogram_svg(bins);
     return 0;
 }
